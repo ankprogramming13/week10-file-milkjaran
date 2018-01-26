4f1e2e6940309ca61619a9572530f18d1ae25aad
Unknown <jaranyaboo@cvc-cha.ac.th>
2018-01-26 13:16:10 +0700

    ทำข้อ 2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main() {
         /* code */
+        string line;
+        ifstream myfile ("example.txt");
+        if (myfile.is_open())
+        {
+            while ( getline (myfile,line))
+            {
+                cout << line << '\n';
+            }
+            myfile.close();
+        }
+        else cout << "Unble to open file";
+
         return 0;
 }
