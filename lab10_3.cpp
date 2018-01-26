5ddfac0e25f24fc1b91edc9d76dd9a5a63e5069d
Unknown <jaranyaboo@cvc-cha.ac.th>
2018-01-26 14:15:52 +0700

    ทำข้อ 3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main() {
+    int cmd;
         /* code */
+        ofstream writefile ("std.txt", ofstream::app);
+        ifstream readFile ("std.txt");
+        string line;
+        string name;
+
+        cin >> cmd;
+        if (cmd== 1) {
+            cin >>name;
+            if (writefile.is_open()) {
+                writefile << name << endl;
+                writefile.close ();
+            } else {
+                cout << "Unable to open file";
+            }
+        }else if (cmd==2) {
+            if (readFile.is_open ()) {
+                while ( getline (readFile,line))
+                    cout << line << '\n';
+            }
+                readFile.close();
+        }
+
         return 0;
 }
